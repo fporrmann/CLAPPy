c79974b9dde3b9264e92fbf8cf4dfada4c40e66f
Florian Porrmann <fporrmann@techfak.uni-bielefeld.de>
2025-05-16 20:23:08 +0200

    v0.1.7  - General version bump  - Added CLAPBufferType into __init__.py

--- CLAPPy/__init__.py
+++ CLAPPy/__init__.py
@@ -6,3 +6,15 @@ except ImportError:
     __version__ = "unknown"
 
 __all__ = ['AxiDMA', 'AxiGPIO', 'AxiInterruptController', 'CLAP', 'CLAPBuffer16s', 'CLAPBuffer16u', 'CLAPBuffer32s', 'CLAPBuffer32u', 'CLAPBuffer64s', 'CLAPBuffer64u', 'CLAPBuffer8s', 'CLAPBuffer8u', 'ClapExp', 'HLSCore', 'Memory', 'MemoryExp', 'VDMA', 'CLAPBufferType']
+
+from typing import Union
+CLAPBufferType = Union[
+	CLAPBuffer8s,
+	CLAPBuffer16s,
+	CLAPBuffer32s,
+	CLAPBuffer64s,
+	CLAPBuffer8u,
+	CLAPBuffer16u,
+	CLAPBuffer32u,
+	CLAPBuffer64u,
+]
